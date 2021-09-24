29460b60fe2b4f8c04f861cdb74ab92724b7d7b6
Mikel <md.ridgeway.work@gmail.com>
2021-09-24 16:43:27 -0500

    React Imports, uniform pragma statements

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity >0.6.0;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/GSN/Context.sol";
